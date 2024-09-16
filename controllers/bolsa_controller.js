5409073fcaf6920a42602cd15b07271cf517be46
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-09-16 08:59:47 -0300

    empece bolsa

--- controllers/bolsa_controller.js
+++ controllers/bolsa_controller.js
@@ -1,1 +1,8 @@
 import { Express } from "express";
+import BolsaService from '../service/bolsa_service'
+const router = express.Router();
+const svc = new BolsaService();
+
+router.get('/bolsa', async, (req, res) => {
+    let response = await 
+})
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import FarmRouter from "./controllers/farm_controller.js";
 import CategoryRouter from "./controllers/category_controller.js"
 import UserRouter from "./controllers/user_controller.js"
 import MedsRouter from "./controllers/meds_controller.js"
+import BolsaRouter from './controllers/bolsa_controller.js'
 
 const app = express();
 const port = 3000;
@@ -16,6 +17,7 @@ app.use('/farmaceutico', FarmRouter);
 app.use('/categorias', CategoryRouter);
 app.use('/usuario', UserRouter);
 app.use('/medicamento', MedsRouter);
+app.use('/bolsa', BolsaRouter)
 
 // Start the server
 app.listen(port, () => {
