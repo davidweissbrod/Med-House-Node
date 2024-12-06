39432ca9e0f6a3e5eb7803c92df7789e21c7b9be
ivan <you@example.comivanfejo06@gmail.com>
2024-12-06 10:05:51 -0300

    messi

--- controllers/pedidos_controller.js
+++ controllers/pedidos_controller.js
@@ -31,8 +31,8 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post('/', auth.authMiddleware, async (req, res) => {
-    let response = await svc.postRequest(req.user.id)
+router.post('/:id', async (req, res) => {
+    let response = await svc.postRequest(req.params.id)
     if(response != null){
         if(response.success){
             return res.status(200).json(response)
--- repositories/pedidos_repository.js
+++ repositories/pedidos_repository.js
@@ -1,120 +1,94 @@
 import DBConfig from '../configs/dbconfig.js';
 import pkg from 'pg';
-const { Client } = pkg;
+const { Pool } = pkg;
+
+const pool = new Pool(DBConfig); // Crear un pool de conexiones con la configuración
 
 export default class PedidosRepository {
-
     // Obtener todos los pedidos de un usuario
     async getAllRequests(userId) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
         const sql = `
-            SELECT id, fecha_pedido, id_usuario FROM public.pedidos WHERE id_usuario = $1`;
+            SELECT * FROM public.pedidos WHERE id_usuario = $1`;
         const values = [userId];
         try {
-            await client.connect();  // Conectar al cliente
-            const result = await client.query(sql, values);  // Ejecutar la consulta
-            // Devolver las filas obtenidas o un array vacío
-            return result.rows.length > 0 ? result.rows : [];
+            const result = await pool.query(sql, values); // Usar pool para la consulta
+            return result.rows.length > 0 ? result.rows : []; // Devolver las filas obtenidas o vacío
         } catch (error) {
             console.error('Error getting requests:', error);
             return [];
-        } finally {
-            await client.end();  // Cerrar la conexión
         }
-    }    
+    }
 
     // Obtener los detalles de un pedido de un usuario
     async getRequestById(id) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
         const sql = `
             SELECT id_medicamento FROM detallepedidos WHERE id_pedidos = $1`;
-        const values = [id];  // Solo necesitamos el id del pedido
+        const values = [id]; // Solo necesitamos el id del pedido
         try {
-            await client.connect();  // Conectar al cliente
-            const result = await client.query(sql, values);  // Ejecutar la consulta
-            // Devolver la fila encontrada o null si no existe
+            const result = await pool.query(sql, values); // Usar pool para la consulta
             return result.rows.length > 0 ? result.rows : null;
         } catch (error) {
             console.error('Error getting request by ID:', error);
             return null;
-        } finally {
-            await client.end();  // Cerrar la conexión
         }
     }
 
     async getRequestInfo(id) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
         const sql = `
             SELECT * FROM pedidos WHERE id = $1`;
-        const values = [id];  // Solo necesitamos el id del pedido
+        const values = [id]; // Solo necesitamos el id del pedido
         try {
-            await client.connect();  // Conectar al cliente
-            const result = await client.query(sql, values);  // Ejecutar la consulta
-            // Devolver la fila encontrada o null si no existe
+            const result = await pool.query(sql, values); // Usar pool para la consulta
             return result.rows.length > 0 ? result.rows : null;
         } catch (error) {
             console.error('Error getting request by ID:', error);
             return null;
-        } finally {
-            await client.end();  // Cerrar la conexión
         }
     }
 
     // Insertar un pedido a la base de datos
     async postRequest(userId) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
-        const sql = 'INSERT INTO public.pedidos (id_usuario, fecha_pedido) VALUES ($1, NOW()) RETURNING *';  // SQL para insertar
-        const values = [userId];  // Solo necesitamos el id del usuario
+        const sql = `
+            INSERT INTO public.pedidos (id_usuario, fecha_pedido) VALUES ($1, NOW()) RETURNING *`; // SQL para insertar
+        const values = [userId]; // Solo necesitamos el id del usuario
         try {
-            await client.connect();  // Conectar al cliente
-            const result = await client.query(sql, values);  // Ejecutar la consulta
-            // Verificar si se inserta correctamente
-            return result.rowCount > 0 ? result.rows[0] : null;  // Devolver la fila insertada o null
+            const result = await pool.query(sql, values); // Usar pool para la consulta
+            return result.rowCount > 0 ? result.rows[0] : null; // Devolver la fila insertada o null
         } catch (error) {
             console.error('Error saving request:', error);
-            return null;  // Retornar null en caso de error
-        } finally {
-            await client.end();  // Cerrar la conexión
+            return null; // Retornar null en caso de error
         }
     }
 
-    async addMedicamentoToDetalle (pedidoId, medId) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
-        const sql = 'INSERT INTO public.detallepedidos (id_medicamento, id_pedidos) VALUES ($1, $2) RETURNING *';  // SQL para insertar
-        const values = [medId, pedidoId];  // Solo necesitamos el id del usuario
+    async addMedicamentoToDetalle(pedidoId, medId) {
+        const sql = `
+            INSERT INTO public.detallepedidos (id_medicamento, id_pedidos) VALUES ($1, $2) RETURNING *`; // SQL para insertar
+        const values = [medId, pedidoId];
         try {
-            await client.connect();  // Conectar al cliente
-            const result = await client.query(sql, values);  // Ejecutar la consulta
-            // Verificar si se inserta correctamente
-            return result.rowCount > 0 ? result.rows[0] : null;  // Devolver la fila insertada o null
+            const result = await pool.query(sql, values); // Usar pool para la consulta
+            return result.rowCount > 0 ? result.rows[0] : null;
         } catch (error) {
-            console.error('Error saving request:', error);
-            return null;  // Retornar null en caso de error
-        } finally {
-            await client.end();  // Cerrar la conexión
+            console.error('Error adding medication to request:', error);
+            return null; // Retornar null en caso de error
         }
     }
 
     // Eliminar todas las búsquedas de un usuario
     async deleteRequest(userId, requestId) {
-        const client = new Client(DBConfig);  // Crear una nueva instancia de Client
-        const sqlDeleteDetalle = 'DELETE FROM public.detallepedidos WHERE id_pedidos = $1';  // Eliminar los detalles del pedido
-        const sqlDeletePedido = 'DELETE FROM public.pedidos WHERE id_usuario = $1 AND id = $2';  // Eliminar el pedido
-        const valuesDetalle = [requestId];  // El id del pedido
-        const valuesPedido = [userId, requestId];  // Los valores para eliminar el pedido
-    
+        const sqlDeleteDetalle = `
+            DELETE FROM public.detallepedidos WHERE id_pedidos = $1`; // Eliminar los detalles del pedido
+        const sqlDeletePedido = `
+            DELETE FROM public.pedidos WHERE id_usuario = $1 AND id = $2`; // Eliminar el pedido
+        const valuesDetalle = [requestId]; // El id del pedido
+        const valuesPedido = [userId, requestId]; // Los valores para eliminar el pedido
+
         try {
-            await client.connect();  // Conectar al cliente
-            // Primero, eliminamos los detalles del pedido
-            await client.query(sqlDeleteDetalle, valuesDetalle);
-            // Luego, eliminamos el pedido
-            const result = await client.query(sqlDeletePedido, valuesPedido);
-            return result.rowCount;  // Retornar la cantidad de filas eliminadas
+            await pool.query(sqlDeleteDetalle, valuesDetalle); // Usar pool para la primera consulta
+            const result = await pool.query(sqlDeletePedido, valuesPedido); // Usar pool para la segunda consulta
+            return result.rowCount; // Retornar la cantidad de filas eliminadas
         } catch (error) {
             console.error('Error deleting request:', error);
-            return 0;  // Retornar 0 en caso de error
-        } finally {
-            await client.end();  // Cerrar la conexión
+            return 0; // Retornar 0 en caso de error
         }
-    }    
+    }
 }
